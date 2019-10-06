b8196e2cb59d11ce9a583e7b024719e9abaec84c
Ozan <bakonong99@gmail.com>
2019-10-06 22:47:22 +0700

    Menambahkan file constant.php

---
+++ constant.php
@@ -0,0 +1,30 @@
+<?php 
+
+	// define('NAMA', 'Rizqi Fauzan');
+
+	// echo NAMA;
+	// echo "<br>";
+
+	// const UMUR = 20;
+	// echo UMUR;
+
+	// class Coba {
+	// 	const NAMA = 'Rizqi Fauzan';
+	// }
+
+	// echo Coba::NAMA;
+
+	// echo __FILE__;
+
+	// function coba(){
+	// 	return __FUNCTION__;
+	// }
+
+	// echo coba();
+ 
+ 	class Coba {
+ 		public $kelas = __CLASS__;
+ 	}
+
+ 	$obj = new Coba;
+ 	echo $obj->kelas;
